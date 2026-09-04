18f5995ce810b6f39ae585b6e179c489b9160e01
niksync1 <40718182+niksync1@users.noreply.github.com>
2026-09-04 13:21:08 +0000

    fix: normalize product service import casing

--- inventory-app/services/index.ts
+++ inventory-app/services/index.ts
@@ -1,5 +1,5 @@
 export { authService, AuthService } from "./AuthService";
-export { productService, ProductService } from "./ProductService";
+export { productService, ProductService } from "./productService";
 export { inventoryService, InventoryService } from "./InventoryService";
 export { transactionService, TransactionService } from "./TransactionService";
 export { reportService, ReportService } from "./ReportService";
